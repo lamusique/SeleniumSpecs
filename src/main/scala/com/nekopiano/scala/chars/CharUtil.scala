47ad4fca96327486bed55f55b57cb076b008f8ad
La Musique <lamusique@users.noreply.github.com>
2018-03-04 09:06:52 +0900

    Distil a test class to a reified class.

---
+++ src/main/scala/com/nekopiano/scala/chars/CharUtil.scala
@@ -0,0 +1,25 @@
+/**
+  * Copyright (c) 2013-2018 nekopiano, Neko Piano
+  * All rights reserved.
+  * http://www.nekopiano.com
+  */
+package com.nekopiano.scala.chars
+
+object CharUtil {
+
+  def isFullWidth(c: Char) = {
+    // To be more precise than bytes or ranges.
+    Character.getName(c) match {
+      // no name coerces to regard as full width
+      case null => true
+      case c if c.startsWith("FULLWIDTH") => true
+      case c if c.startsWith("HALFWIDTH") => false
+      case c if c.startsWith("HIRAGANA") => true
+      case c if c.startsWith("KATAKANA") => true
+      case c if c.startsWith("HANGUL") => true
+      case c if c.startsWith("CJK") => true
+      case _ => false
+    }
+  }
+
+}
